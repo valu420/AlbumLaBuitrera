b907e7a32d443b23da7ec4322d7611000c69f04f
Valu <valentina.sanchez.castrillon@correounivalle.edu.co>
2025-11-14 18:33:31 -0500

    boletines added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Collections from "./pages/Collections";
 import Album from "./pages/Album";
+import Boletines from "./pages/Boletines";
+import BoletinDetalle from "./pages/BoletinDetalle";
 
 function App() {
   return (
@@ -9,6 +11,10 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/colecciones" element={<Collections />} />
       <Route path="/album/:id" element={<Album />} />
+      <Route path="/boletines" element={<Boletines />} />
+      <Route path="/boletin/:id" element={<BoletinDetalle />} />
+      
+    
     </Routes>
   );
 }
--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -1,32 +1,51 @@
 import { Link } from "react-router-dom";
 
 export default function Collections() {
-
   const albums = [
     {
       id: "taller0",
       titulo: "Taller 0",
       portada: "/album/Taller0/portada.jpeg"
     },
-
   ];
 
   return (
-    <div className="album-container">
-      <h1 className="album-title">Colecciones Fotográficas</h1>
+    <>
 
-      <div className="collections-grid">
-        {albums.map(album => (
-          <Link
-            key={album.id}
-            to={`/album/${album.id}`}
-            className="collection-card"
-          >
-            <img src={album.portada} alt="" className="collection-cover" />
-            <h3>{album.titulo}</h3>
-          </Link>
-        ))}
+      {/* NAVBAR */}
+      <nav className="navbar">
+
+        <div className="navbar-left">
+          <h2>Observatorio Ambiental</h2>
+        </div>
+
+        <ul className="navbar-links">
+          <li><Link to="/">Inicio</Link></li>
+          <li><Link to="/colecciones">Colecciones</Link></li>
+          <li><Link to="/boletines">Boletines</Link></li>
+        </ul>
+
+      </nav>
+      {/* FIN NAVBAR */}
+
+
+      <div className="album-container">
+        <h1 className="album-title">Colecciones Fotográficas</h1>
+
+        <div className="collections-grid">
+          {albums.map(album => (
+            <Link
+              key={album.id}
+              to={`/album/${album.id}`}
+              className="collection-card"
+            >
+              <img src={album.portada} alt="" className="collection-cover" />
+              <h3>{album.titulo}</h3>
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
+
+    </>
   );
 }
